e11b4bafdc44f9c5945daeaa8f1dc0422726e148
Charlie Brown <cntvc@outlook.com>
2023-08-31 15:23:11 +0800

    refactor: gacha record table

--- star_rail/module/record/types.py
+++ star_rail/module/record/types.py
@@ -14,10 +14,12 @@ class GachaType(str, enum.Enum):
 GACHA_TYPE_IDS: typing.List[str] = [member.value for member in GachaType]
 """抽卡类型列表"""
 
-GACHA_TYPE_DICT: typing.Dict[str, str] = {
-    GachaType.REGULAR_WARP.value: i18n.regular_warp,
-    GachaType.STARTER_WARP.value: i18n.starter_warp,
-    GachaType.CHARACTER_EVENT_WARP.value: i18n.character_event_warp,
-    GachaType.LIGHT_CONE_EVENT_WARP.value: i18n.light_cone_event_warp,
-}
-"""[抽卡类型:语言字典]"""
+GACHA_TYPE_DICT: typing.OrderedDict[str, str] = typing.OrderedDict(
+    {
+        GachaType.REGULAR_WARP.value: i18n.regular_warp,
+        GachaType.STARTER_WARP.value: i18n.starter_warp,
+        GachaType.CHARACTER_EVENT_WARP.value: i18n.character_event_warp,
+        GachaType.LIGHT_CONE_EVENT_WARP.value: i18n.light_cone_event_warp,
+    }
+)
+"""[抽卡类型ID:抽卡类型名]"""
