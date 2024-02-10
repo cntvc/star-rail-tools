db7d78ce4edda3c1c858f34b11e757045f50d836
cntvc <cntvc@outlook.com>
2024-02-10 22:40:10 +0800

    feat: add app info

--- star_rail/__init__.py
+++ star_rail/__init__.py
@@ -2,4 +2,9 @@ from star_rail.utils.version import get_version
 
 VERSION = (2, 2, 1)
 
+
+__author__ = "cntvc"
+__copyright__ = "Copyright 2024, cntvc"
+__email__ = "cntvc@outlook.com"
+__licence__ = "GPLv3+"
 __version__ = get_version(VERSION)
