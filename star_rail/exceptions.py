247d9a96cf2f73d3f458916692c5d2d0848b350c
cntvc <cntvc@outlook.com>
2024-03-06 22:18:57 +0800

    Revert "test for workflow"
    This reverts commit 59ceef1328b90190b3fd4b6af2913e903409b7dc.

--- star_rail/exceptions.py
+++ star_rail/exceptions.py
@@ -65,7 +65,7 @@ class InvalidLangError(ApiException):
 
 class InvalidCookieError(ApiException):
     retcode = -100
-    msg = "无效的Cookie值."
+    msg = "Invalid cookie value."
 
 
 class VisitsTooFrequently(ApiException):
