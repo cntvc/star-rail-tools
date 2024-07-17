f7f9da247ff7c716df75591fc7b232d17fa74ed0
Charlie Brown <cntvc@outlook.com>
2024-07-18 00:54:25 +0800

    fix: unable to install package

--- star_rail/utils/version.py
+++ star_rail/utils/version.py
@@ -6,6 +6,7 @@ __all__ = ["get_version", "compare_versions", "get_version_tuple"]
 
 def get_version(version: tuple[int, int, int, str, int] = None):
     """Return a PEP 440-compliant version number from VERSION."""
+
     version = get_complete_version(version)
 
     # Build the two parts of the version number:
@@ -21,17 +22,19 @@ def get_version(version: tuple[int, int, int, str, int] = None):
     return main + sub
 
 
-def get_main_version(version: tuple[int, int, int, str, int]):
+def get_main_version(version: tuple[int, int, int, str, int] = None):
     """Return main version (X.Y[.Z]) from VERSION."""
     version = get_complete_version(version)
     parts = 2 if version[2] == 0 else 3
     return ".".join(str(x) for x in version[:parts])
 
 
-def get_complete_version(version: tuple[int, int, int, str, int]):
+def get_complete_version(version: tuple[int, int, int, str, int] = None):
     """
     check for correctness of the tuple provided.
     """
+    if version is None:
+        from star_rail import VERSION as version
     assert len(version) == 5
     assert version[3] in ("dev", "final")
 
