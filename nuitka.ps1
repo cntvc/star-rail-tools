194f5d0dd7b3d4b4a03359a5eba441816c4904cb
Charlie Brown <cntvc@outlook.com>
2024-08-26 06:40:34 +0800

    update

--- nuitka.ps1
+++ nuitka.ps1
@@ -1,10 +1,13 @@
 python -m nuitka `
     star_rail/main.py `
     --standalone `
+    --nofollow-import-to=loguru,pydantic,aiosqlite,pyperclip,xlsxwriter,textual,pycryptodome `
     --output-dir=dist `
     --windows-icon-from-ico=resource/hsr.ico `
-    --nofollow-imports `
-    --follow-import-to=star_rail `
-    --output-filename="StarRailTools" `
+    --output-filename="Launcher" `
     --include-data-dir="star_rail/tui/tcss"="star_rail/tui/tcss" `
-    --include-module=pygments.lexers.javascript `
+    --include-module=aiohttp`
+
+$package_list = @(
+
+)
