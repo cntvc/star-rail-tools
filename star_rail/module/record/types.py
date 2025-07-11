a9f74122a67fde13558dc06fce37b51fc6148988
Charlie Brown <cntvc@outlook.com>
2025-07-11 12:34:07 +0800

    feat:  adds collaboration warp types

--- star_rail/module/record/types.py
+++ star_rail/module/record/types.py
@@ -7,7 +7,9 @@ class GachaRecordType(enum.StrEnum):
     REGULAR_WARP = "1"
     STARTER_WARP = "2"
     CHARACTER_EVENT_WARP = "11"
+    CHARACTER_COLLABORATION_WARP = "21"
     LIGHT_CONE_EVENT_WARP = "12"
+    LIGTH_CONE_COLLABORATION_WARP = "22"
 
     @staticmethod
     def get_by_value(val: str):
@@ -22,9 +24,11 @@ GACHA_TYPE_IDS: list[str] = [member.value for member in GachaRecordType]
 
 GACHA_TYPE_DICT = {
     GachaRecordType.REGULAR_WARP.value: "常驻跃迁",
-    GachaRecordType.STARTER_WARP.value: "新手跃迁",
     GachaRecordType.CHARACTER_EVENT_WARP.value: "角色活动跃迁",
     GachaRecordType.LIGHT_CONE_EVENT_WARP.value: "光锥活动跃迁",
+    GachaRecordType.CHARACTER_COLLABORATION_WARP.value: "角色联动跃迁",
+    GachaRecordType.LIGTH_CONE_COLLABORATION_WARP.value: "光锥联动跃迁",
+    GachaRecordType.STARTER_WARP.value: "新手跃迁",
 }
 
 """[抽卡类型ID:抽卡类型名]"""
