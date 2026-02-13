56290b49fceb204c3e37c688e2e0382e52a96146
Charlie Brown <cntvc@outlook.com>
2026-02-13 16:57:03 +0800

    build: rename dev app desc

--- tui/build.rs
+++ tui/build.rs
@@ -2,13 +2,22 @@ extern crate winresource;
 
 fn main() {
     if cfg!(target_os = "windows") {
+        #[cfg(not(debug_assertions))]
+        let suffix = "";
+        #[cfg(debug_assertions)]
+        let suffix = "-dev";
+
+        let product_name = format!("StarRailTools{}", suffix);
+        let description = format!(
+            "{}{}",
+            std::env::var("CARGO_PKG_DESCRIPTION").unwrap(),
+            suffix
+        );
+
         winresource::WindowsResource::new()
             .set_icon("../assets/srt.ico")
-            .set("ProductName", "StarRailTools")
-            .set(
-                "FileDescription",
-                &std::env::var("CARGO_PKG_DESCRIPTION").unwrap(),
-            )
+            .set("ProductName", &product_name)
+            .set("FileDescription", &description)
             .compile()
             .unwrap();
     }
